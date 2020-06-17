c74dd157fd58433069aadc28f2ed03f206b2a3df
Simon L <slooij1@gmail.com>
2020-06-17 17:39:11 +0100

    Fix TranslatableText objects being formatted while formatting is off

--- src/main/java/me/simon/mixins/TranslatableTextMixin.java
+++ src/main/java/me/simon/mixins/TranslatableTextMixin.java
@@ -1,5 +1,6 @@
 package me.simon.mixins;
 
+import me.simon.Main;
 import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
@@ -20,22 +21,23 @@ public abstract class TranslatableTextMixin  {//removed BaseText - again this ca
 
     @Inject(method = "<init>(Ljava/lang/String;[Ljava/lang/Object;)V", at = @At("RETURN"))//Without "(Ljava/lang/String;[Ljava/lang/Object;)V" the void would not be able to use all the variables
     public void TranslatableText(String key, Object[] args, CallbackInfo ci) {
-            this.key = key;
-            this.args = args;
+        this.key = key;
+        this.args = args;
 
-            for(int i = 0; i < args.length; ++i) {
+        if(Main.settings.enableColor) {
+            for (int i = 0; i < args.length; ++i) {
                 Object object = args[i];
-                if(object instanceof String){
+                if (object instanceof String) {
                     this.args[i] = new LiteralText((String) object);
-                }
-                else if (object instanceof Text) {
-                    Text text = ((Text)object).copy();
+                } else if (object instanceof Text) {
+                    Text text = ((Text) object).copy();
                     this.args[i] = text;
                     //text.getStyle().setParent(this.getStyle()); //I just removed this. It's seemed useless, I might be wrong about that, but that being said removing it allows the mod to compile and run
                 } else if (object == null) {
-                    this.args[i] = "null";
+                       this.args[i] = "null";
                 }
             }
+        }
     }
     @Mutable
     @Final
